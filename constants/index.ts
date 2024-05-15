c8b08caefe379c7e27577676e274019e2f066f2f
Mohau <106928210+JoeyKCS@users.noreply.github.com>
2024-05-15 21:04:23 +0200

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,18 @@
+export const products = [
+  {
+    name: "Futuristic shoes",
+    description: `Step into the future with our revolutionary line of futuristic shoes. 
+    Designed with cutting-edge technology and sleek aesthetics, 
+    these shoes are not just footwear, but an experience. Engineered for comfort, durability, and style, 
+    each pair embodies innovation and sophistication. With advanced materials and precision craftsmanship, our futuristic shoes offer unparalleled performance,
+    whether you're hitting the streets or conquering the gym. 
+    Experience the next generation of footwear and elevate your style with our futuristic shoes today`,
+    images: [
+      "/blueShoes1.png",
+      "/blueShoes4.jpg",
+      "/blueShoes2.png",
+      "/blueShoes3.png",
+    ],
+    price: 250,
+  },
+];
